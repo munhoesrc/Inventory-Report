639193c5ee3292e49d4c0d6ba5d5a0a4bf536d26
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-11 17:44:25 -0300

    feat: 5 - Gere os relatórios através de um arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,9 +7,14 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(file_path: str, report_type: str) -> str:
-        with open(file_path, newline='') as file:
-            reader = csv.DictReader(file)
-            stock = list(reader)
+        with open(file_path, newline="") as file:
+            if file_path.endswith(".csv"):
+                reader = csv.DictReader(file)
+                stock = list(reader)
+            elif file_path.endswith(".json"):
+                stock = json.load(file)
+            else:
+                raise ValueError("Extensão de arquivo inválida")
 
         if report_type == "simples":
             report = SimpleReport.generate(stock)
