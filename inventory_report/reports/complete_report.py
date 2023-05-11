b7a101fba0c9de542d2d3c33e99750fd12ef8867
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-11 16:01:33 -0300

    feat: 3 - Gerar a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+# class CompleteReport:
+#     pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products: list) -> str:
+        resposta = SimpleReport.generate(products)
+
+        produtos_por_empresa = {}
+        for product in products:
+            empresa = product["nome_da_empresa"]
+            produtos_por_empresa[empresa] = (
+                produtos_por_empresa.get(empresa, 0) + 1
+            )
+
+        resposta += "\nProdutos estocados por empresa:\n"
+        for empresa, produto in produtos_por_empresa.items():
+            resposta += f"- {empresa}: {produto}\n"
+
+        return resposta
