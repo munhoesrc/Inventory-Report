f6e765ee00432d405fd6cf25f4cce4cd17a94e92
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-12 19:07:41 -0300

    fix(refatorando a classe Inventory: 6 - Gere os relatórios através de um arquivo XML

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,32 +1,46 @@
 import csv
 import json
 import xml.etree.ElementTree as ET
+
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     @staticmethod
+    def _load_csv(file_path):
+        with open(file_path, newline='') as file:
+            reader = csv.DictReader(file)
+            return [row for row in reader]
+
+    @staticmethod
+    def _load_json(file_path):
+        with open(file_path) as file:
+            return json.load(file)
+
+    @staticmethod
+    def _load_xml(file_path):
+        tree = ET.parse(file_path)
+        stock = []
+        for root in tree.getroot():
+            item = {subroot.tag: subroot.text for subroot in root}
+            stock.append(item)
+        return stock
+
+    @staticmethod
     def import_data(file_path: str, report_type: str) -> str:
-        with open(file_path, newline="") as file:
-            if file_path.endswith(".csv"):
-                reader = csv.DictReader(file)
-                stock = list(reader)
-            elif file_path.endswith(".json"):
-                stock = json.load(file)
-            elif file_path.endswith("xml"):
-                tree = ET.parse(file)
-                root = tree.getroot()
-                stock = []
-                for product in root.findall("product"):
-                    stock.append({
-                        "id": product.get("id"),
-                        "name": product.find("name").text,
-                        "price": product.find("price").text,
-                        "quantity": product.find("quantity").text,
-                    })
-            else:
-                raise ValueError("Extensão de arquivo inválida")
+        loaders = {
+            "csv": Inventory._load_csv,
+            "json": Inventory._load_json,
+            "xml": Inventory._load_xml,
+        }
+
+        extension = file_path.split(".")[-1]
+        if extension not in loaders:
+            raise ValueError("Extensão de arquivo inválida")
+
+        with open(file_path, newline=""):
+            stock = loaders[extension](file_path)
 
         if report_type == "simples":
             report = SimpleReport.generate(stock)
